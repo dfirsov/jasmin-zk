aea7c6a88ac6b64c84940980a14d0f1dba327430
aneto <denis.firsov@gmail.com>
2023-05-12 19:02:06 +0300

    working on the C wrapper

---
+++ src/schnorr_protocol.h
@@ -0,0 +1,28 @@
+#define NLIMBS 32
+
+typedef uint64_t bignum[NLIMBS];
+
+extern void __commitment(
+                         // TODO: or: bignum commitment_p or bignum *commitment_p
+   uint64_t *commitment_p,      /* NLIMBS output  */
+   uint64_t *random_power_p     /* NLIMBS output  */
+);
+
+extern void __response(
+   uint64_t *random_power_p,    /* NLIMBS input */
+   uint64_t *challenge_p,       /* NLIMBS input */
+   uint64_t *witness_p,         /* NLIMBS input (parameter) */
+   uint64_t *response_p         /* NLIMBS output */
+);
+
+extern void __challenge(
+   uint64_t *challenge_p        /* NLIMBS input */
+);
+
+extern void __verify(
+   uint64_t *statement_p,      /* NLIMBS input (parameter) */
+   uint64_t *commitment_p,     /* NLIMBS input */
+   uint64_t *challenge_p,      /* NLIMBS input */
+   uint64_t *response_p,       /* NLIMBS input */
+   uint64_t *result_p          /* 64-word output */
+);
