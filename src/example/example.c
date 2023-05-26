d0d9acca4292ebd495c7113ed6a839561c7cf770
aneto <denis.firsov@gmail.com>
2023-05-26 11:44:28 +0300

    user friendly messages of protocol interaction

--- src/example/example.c
+++ src/example/example.c
@@ -6,16 +6,43 @@
 
 #include "../schnorr_protocol.h"
 
+
+void print(uint64_t *array)
+{
+  int i;
+
+  for(i=0; i<(NLIMBS-1); i++)
+  { printf("0x%016" PRIx64 ", ", array[i]); }
+  printf("0x%016" PRIx64 "\n", array[NLIMBS-1]);
+}
+
 int main(void)
 {
   bignum commitment_p, random_power_p, challenge_p, response_p;
   uint64_t result_p[1];
-  
+
+  printf("\n*** Prover commits: ***\n");
   __commitment(commitment_p, random_power_p);
+  printf("\nCOMMITMENT: ");  
+  print(commitment_p);
+
+  printf("\nSECRET POWER: ");  
+  print(random_power_p);
+
+  printf("\n*** Verifier challenges: ***\n");
   __challenge(challenge_p);
+  printf("\nCHALLENGE: ");  
+  print(challenge_p);
+
+  printf("\n*** Prover responds: ***\n");
   __response(random_power_p, challenge_p, response_p);
+  printf("\nRESPONSE: "); 
+  print(response_p); 
+
+  printf("\n*** Verifier decision: ***\n");
   __verify(commitment_p, challenge_p, response_p, result_p);
 
-  printf("0x%ld\n", result_p[0]);
+  printf("0x%016" PRIx64 "\n\n", result_p[0]);
+
   return 0;
 }
